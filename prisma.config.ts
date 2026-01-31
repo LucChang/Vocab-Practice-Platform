6224b154861d5de39cd977a92085b78bd9a1e02d
LucChang <happyluc1114@gmail.com>
2026-02-01 00:32:57 +0800

    fix: prisma version

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig, env } from 'prisma/config'
 import 'dotenv/config'
 
-const databaseUrl = `${process.env.DATABASE_URL}`
-
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
-    datasource: {
-        url: databaseUrl,
-    },
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
 })
